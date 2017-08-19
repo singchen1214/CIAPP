3edec6b14a2c244aa4e5a6eeea4006fbd81602b9
singchen1214 <singchen1214>
2017-08-19 20:33:47 +0800

    www
    www

---
+++ js/accounting.js
@@ -0,0 +1,30 @@
+
+alert("記錄你想要的東西吧！")
+
+var fdb = new ForerunnerDB();
+var db = fdb.db("mydb");
+var accoutingCollection = db.collection('accounting');
+
+accoutingCollection.load();
+
+$("#submit").click(function() {
+    var date = $("#date").val();
+    var category = $("#category").val();
+    var item = $("#item").val();
+    var cost = $("#cost").val();
+
+    accoutingCollection.insert({
+        date: date,
+        category: category,
+        item: item,
+        cost: cost,
+
+    });
+    accoutingCollection.save();
+alert("儲存成功")
+
+    $("#date").val("");
+    $("#category").val("");
+    $("#item").val("");
+    $("#date").val("");
+});
